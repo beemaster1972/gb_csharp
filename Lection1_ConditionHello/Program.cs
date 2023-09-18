94e4d096c3cef1965decd25aaf7ba669a3fc8533
beemaster1972 <ugor.finno@yandex.ru>
2023-09-18 14:58:53 +0500

    Lection 1 Condition Hello

---
+++ Lection1_ConditionHello/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.Write("Введите имя пользователя: "); 
+string userName = Console.ReadLine();
+if (userName == "BeeMaster")
+{
+    System.Console.WriteLine($"Вы успешно авторизованы, дорогой {userName}!");
+}
+else
+{
+    System.Console.WriteLine($"Вы не авторизованы, НЕ дорогой {userName}!");    
+}

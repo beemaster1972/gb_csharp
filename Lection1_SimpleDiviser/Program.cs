b352f6eac22786fe4e6a4d51dd334875e6794d24
beemaster1972 <ugor.finno@yandex.ru>
2023-09-18 14:34:34 +0500

    Lection 1Simple Diviser

---
+++ Lection1_SimpleDiviser/Program.cs
@@ -0,0 +1,5 @@
+﻿System.Console.Write("Введите первое число A:");
+float numA =Convert.ToSingle(System.Console.ReadLine());
+System.Console.Write("Введите второе число В:");
+float numB =Convert.ToSingle(System.Console.ReadLine());
+System.Console.WriteLine($"Частное от деления {numA} на {numB} = {numA/numB}");

4b5e07ddd3e0c798e9dc6d0649b4a2342d5232ef
beemaster1972 <ugor.finno@yandex.ru>
2023-09-18 14:21:40 +0500

    Lection 1 Simple Calculator

---
+++ Lection1_SimpleCalculator/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите первое число A: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма А и В = {(numA+numB)}");

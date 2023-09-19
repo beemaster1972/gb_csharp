f42650aae37aff1955bb602124abc0519b54bd35
beemaster1972 <ugor.finno@yandex.ru>
2023-09-19 16:44:35 +0500

    Lection 2 Fill Array

---
+++ Lection2_fillArray/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] array = new int[10];
+Random rand = new Random();
+
+void FillArray(int[] collection)
+{
+  int length = collection.Length;
+  int index = -1;
+  while (index++ < length - 1)
+  {
+    // Console.WriteLine($"index = {index}");
+    collection[index] = rand.Next(0, 100);
+  }
+}
+
+
+void printArray(int[] collection)
+{
+  int length = collection.Length;
+  int index = -1;
+  while (index++ < length - 1)
+  {
+    Console.Write($"array[{index}]={collection[index]}{(index < (length - 1) ? ", " : "")}");
+  }
+}
+
+void Main()
+{
+  Console.WriteLine("");
+  FillArray(array);
+  printArray(array);
+  Console.WriteLine("");
+}
+
+Main();
+

6997050b8cf56caad00bb4f88cfaad6b8f75b64b
beemaster1972 <ugor.finno@yandex.ru>
2023-09-21 16:47:06 +0500

    Home work 2. Task 10

--- HomeWorks/homeWork2/Program.cs
+++ HomeWorks/homeWork2/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿// Задача 10: Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+int GetDigit(int number, int digitNumber)
+{
+  // int count = 0;
+  int result;
+  result = number % (10 * *(digitNumber));
+  return result;
+}

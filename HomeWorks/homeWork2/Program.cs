c3a91292cd2bfcdea659ead9d3dec6b56f01fc35
beemaster <ugor.finno@yandex.ru>
2023-09-22 09:28:34 +0500

    Home work 2 tasks 10 and 13

--- HomeWorks/homeWork2/Program.cs
+++ HomeWorks/homeWork2/Program.cs
@@ -3,13 +3,22 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// Одним выстрелом двух зайцев, в смысле две задачи
+
 int GetDigit(int number, int digitNumber)
 {
-  // int count = 0;
+  if (number < Convert.ToInt32(Math.Pow(10,digitNumber)))
+  {
+    return int.MinValue;
+  }
   int result;
-  result = number / Convert.ToInt32(Math.Pow(10, digitNumber));
-  Console.WriteLine(result);
-  result %= Convert.ToInt32(Math.Pow(10,digitNumber-1));
+  result = number % Convert.ToInt32(Math.Pow(10, digitNumber));
+  result /= Convert.ToInt32(Math.Pow(10,digitNumber-1));
   return result;
 }
 
@@ -19,7 +28,8 @@ void Main()
   int number = Convert.ToInt32(Console.ReadLine());
   Console.Write("Enter digit number: ");
   int digitNumber = Convert.ToInt32(Console.ReadLine());
-  Console.WriteLine(GetDigit(number, digitNumber));
+  int answer = GetDigit(number, digitNumber);
+  Console.WriteLine($"{number} -> {(answer == int.MinValue ? $"цифры под номером {digitNumber} нет": answer)}");
 }
 
 Main();

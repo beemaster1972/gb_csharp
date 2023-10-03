a83192fcecd633eb323579d732c84bfb62412d23
beemaster1972 <ugor.finno@yandex.ru>
2023-10-03 16:45:36 +0500

    Lection 4

---
+++ Lections/Lection4/task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Факториал числа
+
+int Factorial(int num)
+{
+  if (num == 1 || num == 0)
+    return 1;
+  return num * Factorial(num - 1);
+}
+
+int num = 10;
+Console.WriteLine($"{num}! = {Factorial(num)}");
---
+++ Lections/Lection4/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Числа Фибоначчи
+//
+int Fibonachi(int num)
+{
+  if (num <= 2)
+    return 1;
+  return Fibonachi(num - 1) + Fibonachi(num - 2);
+}
+
+for (int i = 1; i <= 20; i++)
+{
+  Console.Write($"{Fibonachi(i)} ");
+}

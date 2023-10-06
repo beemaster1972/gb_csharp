f945d9464f5ccc305b3cd90e5775c834d5e43a9d
beemaster1972 <ugor.finno@yandex.ru>
2023-10-06 12:10:50 +0500

    Home work 6

--- HomeWorks/homeWork6/task2/Program.cs
+++ HomeWorks/homeWork6/task2/Program.cs
@@ -3,3 +3,29 @@
 // значения b1, k1, b2 и k2 задаются пользователем.
 //
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double X(double k1, double k2, double b1, double b2)
+{
+    return (b2 - b1) / (k1 - k2);
+}
+
+double Y(double x, double k1, double b1)
+{
+    return k1 * x + b1;
+}
+
+double inputDouble(string promt)
+{
+    Console.Write(promt);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+Console.WriteLine("Для уравнения вида y=k*x+b");
+double k1 = inputDouble("Введите к1: ");
+double b1 = inputDouble("Введите b1: ");
+double k2 = inputDouble("Введите к2: ");
+double b2 = inputDouble("Введите b2: ");
+double x = X(k1, k2, b1, b2);
+double y = Y(x, k1, b1);
+Console.WriteLine($"Для уравнений y={k1}*x+{b1} и y={k2}*x+{b2}");
+Console.WriteLine($"Общая точка имеет координаты ({x}; {y})");

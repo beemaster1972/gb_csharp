f24efb8b4362f60e71a5dc3c0b512a0c368a35fb
beemaster1972 <ugor.finno@yandex.ru>
2023-10-06 12:23:26 +0500

    Home work 6

--- HomeWorks/homeWork6/task2/Program.cs
+++ HomeWorks/homeWork6/task2/Program.cs
@@ -20,12 +20,29 @@ double inputDouble(string promt)
     return Convert.ToDouble(Console.ReadLine());
 }
 
-Console.WriteLine("Для уравнения вида y=k*x+b");
+bool ValidateLines(double k1, double b1, double k2, double b2)
+{
+    if (k1 == k2)
+    {
+        if (b1 == b2)
+            Console.WriteLine("Прямые совдадают");
+        else
+            Console.WriteLine("Прямые параллельны");
+
+        return false;
+    }
+    return true;
+}
+
+Console.WriteLine("Для уравнений вида y=k*x+b");
 double k1 = inputDouble("Введите к1: ");
 double b1 = inputDouble("Введите b1: ");
 double k2 = inputDouble("Введите к2: ");
 double b2 = inputDouble("Введите b2: ");
-double x = X(k1, k2, b1, b2);
-double y = Y(x, k1, b1);
-Console.WriteLine($"Для уравнений y={k1}*x+{b1} и y={k2}*x+{b2}");
-Console.WriteLine($"Общая точка имеет координаты ({x}; {y})");
+if (ValidateLines(k1, b1, k2, b2))
+{
+    double x = X(k1, k2, b1, b2);
+    double y = Y(x, k1, b1);
+    Console.WriteLine($"Для уравнений y={k1}*x+{b1} и y={k2}*x+{b2}");
+    Console.WriteLine($"Общая точка имеет координаты ({x}; {y})");
+}

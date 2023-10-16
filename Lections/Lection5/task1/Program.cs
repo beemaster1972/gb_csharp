d130a659d733cb841790c530ba245d3784c87b71
beemaster1972 <ugor.finno@yandex.ru>
2023-10-16 14:26:45 +0500

    Lection 5

---
+++ Lections/Lection5/task1/Program.cs
@@ -0,0 +1,17 @@
+﻿int n = 1;
+void GetWord(string alphabet, char[] word, int WordLength = 0)
+{
+  string result = String.Empty;
+  if (WordLength == word.Length)
+  {
+    Console.WriteLine($"{n++} {new String(word)}");
+    return;
+  }
+  for (int i = 0; i < alphabet.Length; i++)
+  {
+    word[WordLength] = alphabet[i];
+    GetWord(alphabet, word, WordLength + 1);
+  }
+}
+
+GetWord("abcd", new char[5]);
---
+++ Lections/Lection5/task2/Program.cs
@@ -0,0 +1,16 @@
+﻿void CatalogInfo(string path, string indent = "")
+{
+  DirectoryInfo directory = new DirectoryInfo(path);
+  DirectoryInfo[] catalogs = directory.GetDirectories();
+  foreach (DirectoryInfo catalog in catalogs)
+  {
+    Console.WriteLine($"{indent} [{catalog.Name}]");
+    FileInfo[] files = directory.GetFiles();
+    foreach (FileInfo file in files)
+      Console.WriteLine($"{indent + " "} {file.Name}");
+    CatalogInfo(catalog.FullName, indent + "  ");
+  }
+}
+
+string path = "/home/beemaster/gb/GB_csharp/Lections/";
+CatalogInfo(path);

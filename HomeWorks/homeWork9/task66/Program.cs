8ec73665272b2bda127e707b638a2486adac44a3
beemaster1972 <ugor.finno@yandex.ru>
2023-10-16 16:54:56 +0500

    home work 9

---
+++ HomeWorks/homeWork9/task66/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 66: Задайте значения M и N.
+// Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+//
+
+int GetSumRange(int m, int n)
+{
+    int min = m > n ? n : m;
+    int max = m > n ? m : n;
+    if (min < max)
+    {
+        return GetSumRange(min, max - 1) + max;
+        // System.Console.Write($"{max} ");
+    }
+    else if (min == max)
+        return min;
+    else
+        return 0;
+}
+
+int m = 1;
+int n = 5;
+System.Console.WriteLine($"sum({m};{n}) = {GetSumRange(m, n)}");
---
+++ HomeWorks/homeWork9/task68/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана
+// с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+//
+// функция ack(n, m)
+//    если n = 0
+//      вернуть m + 1
+//    иначе, если m = 0
+//      вернуть ack (n - 1, 1)
+//    еще
+//      вернуть ack(n - 1, ack (n, m - 1))
+
+int Ack(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    else if (m == 0)
+        return Ack(n - 1, 1);
+    else
+        return Ack(n - 1, Ack(n, m - 1));
+}
+
+int n = 2;
+int m = 3;
+System.Console.WriteLine($"Ackerman({n},{m})= {Ack(n, m)}");
+n = 3;
+m = 2;
+System.Console.WriteLine($"Ackerman({n},{m})= {Ack(n, m)}");
